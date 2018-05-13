7c1777e2990ca22b503f6655723530e248fc66ea
Elmalick Ndiaye <elmndiaye@gmail.com>
2018-05-13 12:23:47 -0700

    n

--- logic.js
+++ logic.js
@@ -17,46 +17,46 @@
 
 
 
-$("#submitBtn").on("click", function(event){
-    event.preventDfault();
+$("#add-train").on("click", function(event){
+    event.preventDefault();
 
     var name = $("#trainName").val().trim();
     var destination = $("#destination").val().trim();
-    var nextArival = $("#nextTrain").val().trim();
+    var nextArrival = $("#startTrain").val().trim();
     var trainFreq = $("#frequency").val().trim();
 
 var newTrain = {
     tname: name,
     tdest: destination ,
-    start: nextArival,
-    frequency: trainFreq,
+    start: nextArrival,
+    frequency: (trainFreq)
 };
-
+console.log(newTrain)
 // uploads data to the database
-database.ref().push(newTrain);
+//database.ref().push(newTrain);
 
 // alert user
 alert("Train added");
 
 $("#trainName").val("");
 $("#destination").val("");
-$("#nextTrain").val("");
+$("#startTrain").val("");
 $("#frequency").val("");
 
-});
+//});
 
 
 
 
 
 // Assumptions
-var tFrequency = 3;
+//var tFrequency = 3;
 
-// Time is 3:30 AM
-var firstTime = "03:30";
+// Time is 3:30 AM:
+//var firstTime = "02:00";
 
 // First Time (pushed back 1 year to make sure it comes before current time)
-var firstTimeConverted = moment(firstTime, "HH:mm").subtract(1, "years");
+var firstTimeConverted = moment(newTrain.start, "HH:mm").subtract(1, "years");
 console.log(firstTimeConverted);
 
 // Current Time
@@ -68,11 +68,11 @@ var diffTime = moment().diff(moment(firstTimeConverted), "minutes");
 console.log("DIFFERENCE IN TIME: " + diffTime);
 
 // Time apart (remainder)
-var tRemainder = diffTime % tFrequency;
+var tRemainder = diffTime % newTrain.frequency;
 console.log(tRemainder);
 
 // Minute Until Train
-var tMinutesTillTrain = tFrequency - tRemainder;
+var tMinutesTillTrain = newTrain.frequency - tRemainder;
 console.log("MINUTES TILL TRAIN: " + tMinutesTillTrain);
 
 // Next Train
@@ -80,20 +80,20 @@ var nextTrain = moment().add(tMinutesTillTrain, "minutes");
 console.log("ARRIVAL TIME: " + moment(nextTrain).format("hh:mm"));
 
 
-$("#submitBtn").on("click", function(event){
-    event.preventDfault();
+//$("#submitBtn").on("click", function(event){
+    //event.preventDfault();
 
-    var name = $("#trainName").val().trim();
-    var destination = $("#destination").val().trim();
-    var nextArival = $("#nextTrain").val().trim();
-    var frequency = $("#frequency").val().trim();
+    //var name = $("#trainName").val().trim();
+    //var destination = $("#destination").val().trim();
+    //var nextArival = $("#nextTrain").val().trim();
+    //var frequency = $("#frequency").val().trim();
 
-    $("table > tbody").append("<tr><td>" + name + "</tr></td>");
+    //$("table > tbody").append("<tr><td>" + name + "</tr></td>");
     var tableRow = $("<tr>");
     tableRow.append("<td>" + name + "</td>");
-    tableRow.append("<td>" + destination + "</td>");
-    tableRow.append("<td>" + frequency + "</td>");
-    tableRow.append("<td>" + nextArival + "</td>");
-    tableRow.append("<td>" + minAway + "</td>");
+    tableRow.append("<td>" + newTrain.tdest + "</td>");
+    tableRow.append("<td>" + newTrain.frequency + "</td>");
+    tableRow.append("<td>" + newTrain.start + "</td>");
+    tableRow.append("<td>" + tMinutesTillTrain + "</td>");
     $(".table > tbody").append(tableRow);
-})
+});
